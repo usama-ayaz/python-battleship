a871c9540d64e0d65d512905d53fde5937f01c65
Usama Ayaz <usama.ayaz@live.com>
2022-11-12 01:56:55 +0500

    Remove Unit Test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,11 +1,4 @@
 import unittest
 
-
-class TestSampleClass(unittest.TestCase):
-
-    def test_should_fail(self):
-        self.fail('You should remove this test')
-        
-
 if __name__=="__main__":
     unittest.main()
